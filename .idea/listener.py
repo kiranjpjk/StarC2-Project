c3384e8355f3274c36f0eb84d774d6dd75cdd963
JOGI VENKATA SAI KIRAN <166481628+kiranjpjk@users.noreply.github.com>
2025-11-15 11:49:57 +0530

    Update listener.py

--- .idea/listener.py
+++ .idea/listener.py
@@ -1,29 +1,51 @@
+# import socket
+# import threading
+
+# LISTEN_PORT = 4444
+
+# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# sock.bind(("0.0.0.0", LISTEN_PORT))
+# sock.listen(1)
+
+# print(f"[Linux] Listening on port {LISTEN_PORT}...")
+# client, addr = sock.accept()
+
+# print(f"[+] Connected by Windows agent: {addr}")
+
+# def receive():
+#     while True:
+#         try:
+#             data = client.recv(4096)
+#             if not data:
+#                 break
+#             print("\n[Windows] " + data.decode())
+#         except:
+#             break
+
+# threading.Thread(target=receive, daemon=True).start()
+
+# while True:
+#     msg = input("Linux> ")
+#     client.send(msg.encode())
+
+# linux_chat_server.py
 import socket
-import threading
 
-LISTEN_PORT = 4444
+HOST = "0.0.0.0"
+PORT = 6000
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(("0.0.0.0", LISTEN_PORT))
-sock.listen(1)
+s = socket.socket()
+s.bind((HOST, PORT))
+s.listen(1)
 
-print(f"[Linux] Listening on port {LISTEN_PORT}...")
-client, addr = sock.accept()
-
-print(f"[+] Connected by Windows agent: {addr}")
-
-def receive():
-    while True:
-        try:
-            data = client.recv(4096)
-            if not data:
-                break
-            print("\n[Windows] " + data.decode())
-        except:
-            break
-
-threading.Thread(target=receive, daemon=True).start()
+print("Waiting for Windows connection...")
+client, addr = s.accept()
+print("Connected to:", addr)
 
 while True:
-    msg = input("Linux> ")
-    client.send(msg.encode())
+    msg = client.recv(1024).decode()
+    if msg:
+        print("Windows:", msg)
+    reply = input("Linux: ")
+    client.send(reply.encode())
+//
